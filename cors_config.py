6160b3889b66bf325accf10fb4f72b284bbced8b
FBAgol <farzadgolzari1999@gmail.com>
2025-02-03 15:44:31 +0100

    sdsds

--- cors_config.py
+++ cors_config.py
@@ -4,7 +4,7 @@ def add_cors_middleware(app):
     
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173"],  # die URLs, die auf die API zugreifen dürfen
+        allow_origins=["http://localhost:5173"],  # die URLs, die auf die API zugreifen dürfen   
         allow_credentials=True,
         allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Erlaubte HTTP-Methoden
         allow_headers=["Content-Type", "Authorization"],  # Erlaubte Header zum beispiel für token oder Content-Type
